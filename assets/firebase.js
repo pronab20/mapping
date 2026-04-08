1f2a42cd9eb69d0d11069424cdc4c57d25b662bd
pronab20 <70341622+pronab20@users.noreply.github.com>
2026-04-08 22:11:31 +0530

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -5,7 +5,11 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 const firebaseConfig = {
   apiKey: "AIzaSyDx9fB5OFk23fNCvTBu6XIfqlNuqfKbdp0",
   authDomain: "rcplcso.firebaseapp.com",
-  projectId: "rcplcso"
+  projectId: "rcplcso",
+  storageBucket: "rcplcso.firebasestorage.app",
+  messagingSenderId: "515368672745",
+  appId: "1:515368672745:web:83afb963806a68a008bf78",
+  measurementId: "G-BLDNTZLFRS"
 };
 
 const app = initializeApp(firebaseConfig);
