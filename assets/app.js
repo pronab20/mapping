562f609355aaa73f21b78f3015813c1e88869602
pronab20 <70341622+pronab20@users.noreply.github.com>
2026-04-08 22:31:36 +0530

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,10 @@
-
 import { db } from "./firebase.js";
 import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 window.addRow = function(){
     let t = document.getElementById("dist_table");
     let r = t.insertRow();
+
     r.innerHTML = `
         <td><input class="dbr_id"></td>
         <td><input class="dbr_name"></td>
@@ -13,9 +13,12 @@ window.addRow = function(){
 };
 
 window.saveData = async function(){
+
     let distributors = [];
+
     document.querySelectorAll("#dist_table tr").forEach((row,i)=>{
         if(i===0) return;
+
         distributors.push({
             id: row.querySelector(".dbr_id").value,
             name: row.querySelector(".dbr_name").value,
@@ -32,14 +35,10 @@ window.saveData = async function(){
         distributors
     });
 
-    alert("Saved Successfully");
+    alert("Saved Successfully ✅");
 };
 
-await addDoc(collection(db, "cso_data"), {
-    cso_id: document.getElementById("cso_id").value,
-    rev: document.getElementById("rev").value,
-    cooler: document.getElementById("cooler").value,
-    outlet: document.getElementById("outlet").value,
-    ase_id: localStorage.getItem("user"),
-    distributors
-});
+window.logout = function(){
+    localStorage.clear();
+    window.location = "index.html";
+};
